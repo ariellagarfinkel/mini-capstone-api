221b19bf2aff3f79bec831687d6e927dc1aad77f
Ariella Garfinkel <ariellagarfinkel@gmail.com>
2023-07-13 15:06:05 -0500

    Added a Supplier model

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,7 @@
 # product.save
 # product = Product.new(name: "waterbottle", price: 2, image_url: "https://media.gcflearnfree.org/content/55e0730c7dd48174331f5164_01_17_2014/whatisacomputer_desktop_computers.jpg", description: "something to drink")
 # product.save
+
+# supplier = Supplier.create(name: "Nike", email: "nike@gmail.com", phone_number: "12345")
+# supplier = Supplier.create(name: "adidas", email: "adidas@gmail.com", phone_number: "123456")
+# supplier = Supplier.create(name: "under_armour", email: "underarmour@gmail.com", phone_number: "1234567")
