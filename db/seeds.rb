7fce4e81b6563fcfa3ef7616c3db7617d6df266f
Ariella Garfinkel <ariellagarfinkel@gmail.com>
2023-08-25 13:37:54 -0500

    deleted image_url from original seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-product = Product.new(name: "computer", price: 500, image_url: "https://media.gcflearnfree.org/content/55e0730c7dd48174331f5164_01_17_2014/whatisacomputer_desktop_computers.jpg", description: "A device that computes")
+product = Product.new(name: "computer", price: 500, description: "A device that computes")
 product.save
-product = Product.new(name: "waterbottle", price: 2, image_url: "https://media.gcflearnfree.org/content/55e0730c7dd48174331f5164_01_17_2014/whatisacomputer_desktop_computers.jpg", description: "something to drink")
+product = Product.new(name: "waterbottle", price: 2, description: "something to drink")
 product.save
 
 supplier = Supplier.create(name: "Nike", email: "nike@gmail.com", phone_number: "12345")
