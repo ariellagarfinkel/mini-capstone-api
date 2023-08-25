9c30308b4f27dee02e9dd95e896510c08584a6b0
Ariella Garfinkel <ariellagarfinkel@gmail.com>
2023-08-25 14:06:19 -0500

    fixed typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-product = Product.create!(name: "computer", price: 500, description: "A device that computes")
-
-product = Product.create!(name: "waterbottle", price: 2, description: "something to drink")
-
 supplier = Supplier.create!(name: "Nike", email: "nike@gmail.com", phone_number: "12345")
 supplier = Supplier.create!(name: "adidas", email: "adidas@gmail.com", phone_number: "123456")
 supplier = Supplier.create!(name: "under_armour", email: "underarmour@gmail.com", phone_number: "1234567")
+
+product = Product.create!(name: "computer", price: 500, description: "A device that computes", quantity: 1, supplier_id: 1)
+
+product = Product.create!(name: "waterbottle", price: 2, description: "something to drink", quantity: 1, supplier_id: 1)
 
 Image.create!(url: "https://t4.ftcdn.net/jpg/01/36/70/67/360_F_136706734_KWhNBhLvY5XTlZVocpxFQK1FfKNOYbMj.jpg", product_id: Product.first.id)
 
